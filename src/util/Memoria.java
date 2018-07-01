518a3e01872c3f22a34d29f3897422ec3bd90cb4
Kenedy <Siga Mais@DESKTOP-MQ0DKDS>
2018-07-01 01:06:22 -0300

    Comentários

--- src/util/Memoria.java
+++ src/util/Memoria.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Memoria {
-	private int[] memoria = new int[512];
+	private int[] memoria = new int[512]; // Memória de 512 MB
 	private ArrayList<Processo> processos = new ArrayList<>();;
 
+	// Criação dos processos
 	public Memoria() {
 		for (int i = 0; i < 15; i++) {
 			Random r = new Random();
@@ -14,19 +15,22 @@ public class Memoria {
 		}
 	}
 
+	// Alocação dos processos na memória
 	public void run() {
 		for (Processo processo : processos) {
 			if (existememoria(processo.getTamanho())) {
+				// Executar algoritmo de alocação nessa parte
+				
 				if (isCompactar(processo.getTamanho())) {
 					compactar();
 				}
-
 			} else {
 				System.out.println("Sem Memória");
 			}
 		}
 	}
 
+	// Verificação de espaço de memória livre
 	private boolean existememoria(int espaco) {
 		int countlivre = 0;
 		for (int i = 0; i < memoria.length; i++) {
@@ -40,6 +44,7 @@ public class Memoria {
 			return false;
 	}
 
+	// Verificação da memória para saber se existe espaço para inserir processo
 	private boolean isCompactar(int tamanho) {
 		int sequencia = 0;
 		for (int i = 0; i < memoria.length; i++) {
@@ -51,25 +56,32 @@ public class Memoria {
 			if (sequencia >= tamanho)
 				return false;
 		}
+		// Caso chegue no final da memória e o processo não for alocado, realizar
+		// compactação
 		return true;
 	}
 
+	// Compactação
 	private void compactar() {
 		ArrayList<Integer> aux = new ArrayList<>();
 
+		// Copia toda a memória para um auxiliar
 		for (int i = 0; i < this.memoria.length; i++) {
 			if (this.memoria[i] == 0)
 				aux.add(memoria[i]);
 		}
 
+		// Cria uma memória nova
 		int[] memoria = new int[512];
 		int count = 0;
 
+		// Insere os dados
 		for (Integer i : aux) {
 			memoria[count] = i;
 			count++;
 		}
 
+		// Substitui a memória
 		this.memoria = memoria;
 	}
 }

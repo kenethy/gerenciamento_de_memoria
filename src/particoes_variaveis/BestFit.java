6c55012853bbd292ca66927652ea41722e18fea9
cesar <cesarmarmun@gmail.com>
2018-07-05 00:43:13 -0300

    bestFit

--- src/particoes_variaveis/BestFit.java
+++ src/particoes_variaveis/BestFit.java
@@ -1,10 +1,55 @@
 package particoes_variaveis;
 
+import util.Memoria;
+import util.Processo;
+
+
 /**
  * @author César Muniz
  *
  */
 
-public class BestFit {
 
+public class BestFit extends Memoria {
+	
+	@Override
+	public void runTipo(Processo processo) {
+		boolean flag = true;
+		int indiceMenor = memoria.length;
+		int qtdMenor = memoria.length;
+		int auxIndice = 0;
+		int auxQtd = 0;
+		
+		if (isCompactar(processo.getTamanho())) {
+			System.out.println("Sem Memória, Compactando...");
+			compactar();
+		}
+		
+		for (int i = 0; i < memoria.length; i++) {
+			
+			if(memoria[i] == 0) {				
+				if(flag) {					
+					auxIndice = i;
+					flag = false;
+				}
+				auxQtd++;			
+			}
+			if(memoria[i] != 0 || i == 511) {				
+				flag = true;
+				if(auxQtd >= processo.getTamanho()) {
+					if(auxQtd <= qtdMenor) {						
+						indiceMenor = auxIndice;
+						qtdMenor = auxQtd;
+					}
+				}							
+			}			
+		}
+//		System.out.println(indiceMenor);
+		
+		for(int i=indiceMenor; i<(processo.getTamanho()+indiceMenor); i++) {
+			memoria[i] = processo.getId();
+		}
+		
+	}
+	
 }
